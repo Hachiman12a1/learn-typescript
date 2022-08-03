e954b797959b79c9c257ca9492ff1924118f5a58
Dao Van Dat <daovandat2k15@gmail.com>
2022-08-03 21:12:38 +0700

    Typescript: 03-02 Type Alias and Interface 🔥

---
+++ src/lectures/03-02.ts
@@ -0,0 +1,56 @@
+interface BusinessPartner {
+  name: string;
+  credit: number;
+}
+interface Identity {
+  id: number;
+  name: string;
+}
+interface Contact {
+  email: string;
+  phone: string;
+}
+// type Employee = Identity & Contact;
+// type Customer = BusinessPartner & Contact;
+
+// interface way
+// interface Employee extends Identity, Contact {}
+// interface Customer extends BusinessPartner, Contact {}
+
+type Customer = BusinessPartner & Contact;
+let c: Customer = {
+  name: "ABC Inc.",
+  credit: 1000000,
+  email: "sales@abcinc.com",
+  phone: "(408)-897-5735",
+};
+type Employee = Identity & BusinessPartner & Contact;
+let e: Employee = {
+  id: 100,
+  name: "John Doe",
+  email: "john.doe@example.com",
+  phone: "(408)-897-5684",
+  credit: 1000,
+};
+
+interface Student {
+  id: number;
+  name: string;
+}
+// declaration merging
+// works, no error
+interface Student {
+  age?: number;
+}
+const alice: Student = {
+  id: 1,
+  name: "Alice",
+};
+
+// type Student1 = {
+//     id: number;
+//     name: string;
+//     }
+//     // ts error: Duplicate identifier 'Student1'.
+//     type Student1 = {
+//     age?:
